e2c7a308a123ffb7f07df7f658cb526f8526ad77
shukri1236 <shukri1236>
2024-07-20 23:34:12 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+
+
+function submitData(userName, userEmail) {
+    const userInfo = {
+      name: userName,
+      email: userEmail
+    };
+  
+    const configUser = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(userInfo)
+    };
+  
+    return fetch('http://localhost:3000/users', configUser)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        document.body.innerHTML = data.id;
+      })
+      .catch(error => {
+        document.body.innerHTML = `Error: ${error.message}`;
+      });
+  }
